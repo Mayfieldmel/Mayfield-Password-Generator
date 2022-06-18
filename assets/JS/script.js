1da8397fc211e693535d1066dc47d95a263e0dbe
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-06-18 18:13:46 -0400

    retry prompt if outside length parameters

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,5 +1,30 @@
 // Assignment code here
 
+// generate password = randomized characters for selected length
+// randomized characters
+// var character = {
+//   letters: ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz,
+//   numbers: 0123456789,
+//   length: window.prompt("How long is your password?")
+// }
+
+function generatePassword() {
+  var passwordLength = window.prompt ("How long is your password? Please pick a number between 8 and 128");
+
+  if (passwordLength >= 8  && passwordLength <= 128) {
+
+  } else {
+  window.alert ("Your password must be between 8 and 128 characters long. Please pick another number.");
+  generatePassword();
+  }
+
+  console.log(passwordLength);
+}
+
+  
+
+
+
 
 
 // Get references to the #generate element
