752269e0b0b20fbce5c68d233668186a9cca19b2
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-06-19 13:33:08 -0400

    randomNumber function

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,133 +3,162 @@
 // generate password = randomized characters for selected length
 // character.letters = Math.floor(Math.random())
 
-var character = {
-  letters: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
-  numbers: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-  specials: ["!", "@", "#", "$", "%", "^", ")", "&", "*", "_", ";", "'", ":", "|", ",", ".", "<", ">", "?", "+", "-"],
+
+// var letter = {
+//   characters: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
+//   length: 26,
+// }
+
+// var number = {
+//   characters: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+//   length: 10,
+// }
+
+// var special = {
+//   characters: ["!", "@", "#", "$", "%", "^", "&", "*", "_", ";", "'", ":", "|", ",", ".", "<", ">", "?", "+", "-"],
+//   length: 20
+// }
+
+var confirmAll = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "^", "&", "*", "_", ";", "'", ":", "|", ",", ".", "<", ">", "?", "+", "-"];
+var confirmAllLength = 56;
+
+
+
+var randomNumber = function() {
+  var value = Math.floor(Math.random() * confirmAllLength);
+  console.log(value);
+  console.log(confirmAll[value]);
+  return value;
 }
 
+randomNumber();
 
 
-var passwordLength = window.prompt("How long is your password? Please pick a number between 8 and 128")
+// var passwordLength = function() {
+//   window.prompt("How long is your password? Please pick a number between 8 and 128")
+// }
+
+// passwordLength();
 
 
-// Ask for passwordLength
-console.log(character);
-  // if passwordLength is between 8 and 128
-  if (passwordLength >= 8  && passwordLength <= 128) {
-    console.log(passwordLength)
 
-    // Ask for uppercase letters
-    var confirmUpperCase = window.confirm("Would you like to use uppercase letters in your password?")
+// // Ask for passwordLength
 
-    // if user confirms uppercase letters
-    if (confirmUpperCase) {
-      // Ask for lowercase letters
-      var confirmLowerCase = window.confirm("Would you like to use lowercase letters in your password?")
-      // if user confirms lowercase letters
-      if (confirmLowerCase) {
-        console.log("uppercase & lowercase")
-        // Ask for numbers
-        var confirmNumbers = window.confirm("Would you like numbers in your password?")
-        // if user confirms numbers
-        if (confirmNumbers) {
-          console.log("numbers")
-          // Ask for special characters
-          var confirmSpecials = window.confirm("Would you like your password to contain special characters?")
-          // if user confirms special characters
-          if (confirmSpecials) {
-            console.log("special characters")
-          
-          // if users refuses special characters
-          } else {
-            console.log("no special characters")
-          }
+//   // if passwordLength is between 8 and 128
+//   if (passwordLength >= 8  && passwordLength <= 128) {
+//     console.log(passwordLength)
 
-        // if user refuses numbers
-        } else {
-          console.log("no numbers")
-          // Ask for special characters
-          var confirmSpecials = window.confirm("Would you like your password to contain special characters?")
-          // if user confirms special characters
-          if (confirmSpecials) {
-            console.log("special characters")
-          // if users refuses special characters
-          } else {
-            console.log("no special characters")
-          }
-        }
+//     // Ask for uppercase letters
+//     var confirmUpperCase = window.confirm("Would you like to use uppercase letters in your password?")
 
-      // if user refuses lowercase letters
-      } else {
-        console.log("only uppercase")
-        // Ask for numbers 
-        var confirmNumbers = window.confirm("Would you like numbers in your password?")
-        // if confirm numbers
-        if (confirmNumbers) {
-          console.log("numbers")
-          // Ask for special characters
-          var confirmSpecials = window.confirm("Would you like your password to contain special characters?")
-          // if confirm special characters
-          if (confirmSpecials) {
-            console.log("special characters")
-          // if refuses special characters
-          } else {
-            console.log("no special characters")
-          }
+//     // if user confirms uppercase letters
+//     if (confirmUpperCase) {
+//       // Ask for lowercase letters
+//       var confirmLowerCase = window.confirm("Would you like to use lowercase letters in your password?")
+//       // if user confirms lowercase letters
+//       if (confirmLowerCase) {
+//         console.log("uppercase & lowercase")
+//         // Ask for numbers
+//         var confirmNumbers = window.confirm("Would you like numbers in your password?")
+//         // if user confirms numbers
+//         if (confirmNumbers) {
+//           console.log("numbers")
+//           // Ask for special characters
+//           var confirmSpecials = window.confirm("Would you like your password to contain special characters?")
+//           // if user confirms special characters
+//           if (confirmSpecials) {
+//             console.log("special characters");
+           
+//           // if users refuses special characters
+//           } else {
+//             console.log("no special characters")
+//           }
 
-        // if user refuses numbers
-        } else {
-          console.log("no numbers")
-          // Ask for special characters
-          var confirmSpecials = window.confirm("Would you like your password to contain special characters?")
-          // if confirm special characters
-          if (confirmSpecials) {
-            console.log("special characters")
-          // if user refuses special characters
-          } else {
-            console.log("no special characters")
-          }
-        }
-      }
+//         // if user refuses numbers
+//         } else {
+//           console.log("no numbers")
+//           // Ask for special characters
+//           var confirmSpecials = window.confirm("Would you like your password to contain special characters?")
+//           // if user confirms special characters
+//           if (confirmSpecials) {
+//             console.log("special characters")
+//           // if users refuses special characters
+//           } else {
+//             console.log("no special characters")
+//           }
+//         }
 
-    // if users refuses uppercase letters
-    } else {
-      console.log("only lowercase")
-      // Ask for numbers
-      var confirmNumbers = window.confirm("Would you like numbers in your password?")
-        // if confirm numbers
-        if (confirmNumbers) {
-          console.log("numbers")
-          // Ask for special characters
-          var confirmSpecials = window.confirm("Would you like your password to contain special characters?")
-          // if user confirms special characters
-          if (confirmSpecials) {
-            console.log("special characters")
-          // if user refuses special characters
-          } else {
-            console.log("no special characters")
-          }
+//       // if user refuses lowercase letters
+//       } else {
+//         console.log("only uppercase")
+//         // Ask for numbers 
+//         var confirmNumbers = window.confirm("Would you like numbers in your password?")
+//         // if confirm numbers
+//         if (confirmNumbers) {
+//           console.log("numbers")
+//           // Ask for special characters
+//           var confirmSpecials = window.confirm("Would you like your password to contain special characters?")
+//           // if confirm special characters
+//           if (confirmSpecials) {
+//             console.log("special characters")
+//           // if refuses special characters
+//           } else {
+//             console.log("no special characters")
+//           }
 
-        // If user refuses numbers 
-        } else {
-          console.log("no numbers")
-          // Ask for special characters
-          var confirmSpecials = window.confirm("Would you like your password to contain special characters?")
-          // if confirm special characters
-          if (confirmSpecials) {
-            console.log("special characters")
-          // if user refuses special characters
-          } else {
-            console.log("no special characters")
-          }
-        }
-    }
+//         // if user refuses numbers
+//         } else {
+//           console.log("no numbers")
+//           // Ask for special characters
+//           var confirmSpecials = window.confirm("Would you like your password to contain special characters?")
+//           // if confirm special characters
+//           if (confirmSpecials) {
+//             console.log("special characters")
+//           // if user refuses special characters
+//           } else {
+//             console.log("no special characters")
+//           }
+//         }
+//       }
 
-  // if passwordLength is not between 8 and 128
-  } else {
-    window.alert ("Your password must be between 8 and 128 characters long. Please pick another number.");
-    }
+//     // if users refuses uppercase letters
+//     } else {
+//       console.log("only lowercase")
+//       // Ask for numbers
+//       var confirmNumbers = window.confirm("Would you like numbers in your password?")
+//         // if confirm numbers
+//         if (confirmNumbers) {
+//           console.log("numbers")
+//           // Ask for special characters
+//           var confirmSpecials = window.confirm("Would you like your password to contain special characters?")
+//           // if user confirms special characters
+//           if (confirmSpecials) {
+//             console.log("special characters")
+//           // if user refuses special characters
+//           } else {
+//             console.log("no special characters")
+//           }
+
+//         // If user refuses numbers 
+//         } else {
+//           console.log("no numbers")
+//           // Ask for special characters
+//           var confirmSpecials = window.confirm("Would you like your password to contain special characters?")
+//           // if confirm special characters
+//           if (confirmSpecials) {
+//             console.log("special characters")
+//           // if user refuses special characters
+//           } else {
+//             console.log("no special characters")
+//           }
+//         }
+//     }
+
+//   // if passwordLength is not between 8 and 128
+//   } else {
+//     window.alert ("Your password must be between 8 and 128 characters long. Please pick another number.");
+//     passwordLength();
+//     }
   
 
 
